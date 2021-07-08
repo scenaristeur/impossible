fb4b2cb754994273ad229d565f08ca5a0ec295c0
Scenaristeur <scenaristeur@gmail.com>
2021-07-08 20:17:35 +0200

    first test

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    Pour qu'aujourd’hui, les Archipels s’activent en Symbiose !
+remplissez ci-dessous un premier formulaire pour FAIRE connaissance
+
+Pour une première prise de contact, sans aucune contrainte nous vous proposons de renseigner le formulaire ci-dessous afin de commencer à faire connaissance.
+Vous pouvez aussi entrer en communication directement par mél pour que nous convenions d'un temps commun en visio autour de nos démarches en Archipels
     <b-button >Envoyer</b-button>
   </div>
 </template>
